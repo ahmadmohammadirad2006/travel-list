340c7af78c97d6f3c6a3c5e725909cb5f8a21c4e
ahmadmohammadirad2006 <ahmadmohammadirad2006@gmail.com>
2024-09-14 15:51:50 +0330

    feat: implement adding new item functionality
     add handleAddItems function into App component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,18 +27,15 @@ function Logo() {
   return <h1>🏝️ Far Away bag 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
-
-  console.log(quantity);
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
-
+    onAddItems(newItem);
     setDescription('');
     setQuantity(1);
   }
@@ -61,11 +64,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className='list'>
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
